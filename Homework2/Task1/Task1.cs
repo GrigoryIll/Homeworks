00774802fcf5a7db07368b5fd425db5b3b909b92
Grigory <grigory@i.ru>
2023-01-20 20:08:07 +0300

    дз на проверку

---
+++ Homework2/Task1/Task1.cs
@@ -0,0 +1,18 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+int GetRandomValue()
+{
+return new Random().Next(100, 1000);
+}
+
+int ShowSecondNumber(int number)
+{
+return (number % 100) / 10 ;
+}
+
+int number = GetRandomValue();
+int result = ShowSecondNumber(number);
+Console.WriteLine($"Было {number}, стало {result}.");
