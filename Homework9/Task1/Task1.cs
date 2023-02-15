1dcd488629e079bcc89792bfc6bb83dba8a70315
Grigory <grigory@i.ru>
2023-02-15 12:40:35 +0300

    дз на проверку

---
+++ Homework9/Task1/Task1.cs
@@ -0,0 +1,34 @@
+﻿/* Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
+
+int GetNumber (string message)
+{
+    int result = 0;
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число. Повторите ввод");
+        }
+    }
+    return result;
+}
+
+void NumberInRecursion(int number)
+{
+    if (number > 0)
+    {
+        Console.Write($"{number} ");
+        number = number - 1;
+        NumberInRecursion(number);
+    }
+}
+int number = GetNumber("Введите число");
+NumberInRecursion(number);
---
+++ Homework9/Task2/Task2.cs
@@ -0,0 +1,17 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+int SumOfNumbers(int m, int n, int sum, int i)
+{
+    while (i >= m && i <= n)
+    { 
+        sum = sum + i;
+        i++;
+    }
+    return sum; 
+}
+
+int result = SumOfNumbers(4, 8, 0, 4);
+Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N равно: {result}");
